67f1a88aaa8b2e9722c269fca7be5557bf39b2c3
Kartik Sharma ( Krishna ) <kartikkrishnasharmaa@gmail.com>
2025-09-24 18:46:18 +0530

    Update max width of container in branch.js

--- src/pages/sinfodeadmin/2_branch/branch.js
+++ src/pages/sinfodeadmin/2_branch/branch.js
@@ -328,7 +328,7 @@ export default function Branch() {
         theme="light"
       />
       
-      <div className="max-w-7xl mx-auto p-6 w-full">
+      <div className="max-w-9xl mx-auto p-6 w-full">
         {/* Header */}
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-6 gap-4">
           <h1 className="text-3xl md:text-[34px] font-nunito font-bold">
