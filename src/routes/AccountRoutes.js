4b896da27ea1f2dd103d7b1ce0c08072adb3828b
Kartik Sharma ( Krishna ) <kartikkrishnasharmaa@gmail.com>
2025-09-30 16:52:02 +0530

    Add Attendence route to AccountRoutes

--- src/routes/AccountRoutes.js
+++ src/routes/AccountRoutes.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
 import Accountdashboard from "../pages/accounts/dashboard/dashboard";
 import Profile from "../pages/accounts/dashboard/profile";
+import Attendence from "../pages/accounts/attendence/myattendence"
 
 import Fees from "../pages/accounts/5_fees/fees";
 
@@ -21,6 +22,14 @@ const AccRoutes = () => {
         element={
           <ProtectedRoute>
             <Profile />
+          </ProtectedRoute>
+        }
+      />
+        <Route
+        path="/account/attendence"
+        element={
+          <ProtectedRoute>
+            <Attendence />
           </ProtectedRoute>
         }
       />
