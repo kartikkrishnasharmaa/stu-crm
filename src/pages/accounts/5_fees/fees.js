201dfcf643140f1d61d368df25cbb21ffdc4eec0
Kartik Sharma ( Krishna ) <kartikkrishnasharmaa@gmail.com>
2025-10-06 08:56:38 +0530

    Clean up Fees component by removing unused imports
    Removed unused components and commented-out code from Fees component.

--- src/pages/accounts/5_fees/fees.js
+++ src/pages/accounts/5_fees/fees.js
@@ -1,9 +1,7 @@
 import SMLayout from "../../../layouts/AccountLayout";
 import { useState } from "react";
-import Dashboard from "./dashboard";
-import FeesStructure from "./structure";
+
 import Collection from "./collection";
-import GenerateFees from "./generatefees";
 import Reminder from "./remider";
 
 export default function Fees() {
@@ -14,36 +12,7 @@ export default function Fees() {
       <div className="flex h-full">
         {/* Sidebar */}
         <div className="w-60 bg-white rounded-xl shadow-md p-4 space-y-3">
-          {/* <button
-            onClick={() => setActiveTab("dashboard")}
-            className={`block w-full text-left px-4 py-5 rounded-lg ${
-              activeTab === "dashboard"
-                ? "bg-blue-100 text-black"
-                : "hover:bg-blue-100 text-black"
-            }`}
-          >
-            📋 Dashboard
-          </button> */}
-          {/* <button
-            onClick={() => setActiveTab("fees")}
-            className={`block w-full text-left px-4 py-5 rounded-lg ${
-              activeTab === "fees"
-                ? "bg-blue-100 text-black"
-                : "hover:bg-blue-100 text-black"
-            }`}
-          >
-            💰 Fees Structure
-          </button> */}
-          {/* <button
-            onClick={() => setActiveTab("generate")}
-            className={`block w-full text-left px-4 py-5 rounded-lg ${
-              activeTab === "generate"
-                ? "bg-blue-100 text-black"
-                : "hover:bg-blue-100 text-black"
-            }`}
-          >
-            🛠️ Generate Fees
-          </button> */}
+         
           <button
             onClick={() => setActiveTab("collection")}
             className={`block w-full text-left px-4 py-5 rounded-lg ${
@@ -68,9 +37,6 @@ export default function Fees() {
 
         {/* Content */}
         <div className="flex-1 rounded-lg p-6 overflow-y-auto">
-          {/* {activeTab === "dashboard" && <Dashboard />} */}
-          {/* {activeTab === "fees" && <FeesStructure />} */}
-          {/* {activeTab === "generate" && <GenerateFees />} */}
           {activeTab === "collection" && <Collection />}
           {activeTab === "reminder" && <Reminder />}
         </div>
