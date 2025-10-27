3efa1195be1879e113fb74f4ba3e656f1b3efb47
Kartik Sharma ( Krishna ) <kartikkrishnasharmaa@gmail.com>
2025-10-27 17:56:48 +0530

    Change axios base URL for testing environment

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'https://crmm.sinfode.com/api/',
+  baseURL: 'https://crmtesting.sinfode.com/api/',
+  // baseURL: 'https://crmm.sinfode.com/api/',
   headers: {
     'Content-Type': 'application/json',
   },
